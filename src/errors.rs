d9518a9bff31201145c348bbba29eaf0509d8781
ron <yrong1997@gmail.com>
2023-12-21 20:52:26 +0800

    Introduce RuntimeDebug

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,8 @@
-#[derive(Debug, PartialEq, Clone)]
+use codec::{Decode, Encode, MaxEncodedLen};
+use scale_info::TypeInfo;
+use sp_core::RuntimeDebug;
+
+#[derive(PartialEq, Clone, Encode, Decode, TypeInfo, MaxEncodedLen, RuntimeDebug)]
 pub enum AmclError {
     AggregateEmptyPoints,
     HashToFieldError,
